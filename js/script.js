38e66939e0f1c93dbcc3db6802a05e08cb5167bc
muhamedkrbalo <mohamedkrbalo@gmail.com>
2023-05-16 13:17:51 +0300

    lv

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// dir
 var bodyDir = $('body').css('direction')
 console.log(bodyDir)
 var dirAr
@@ -8,150 +9,103 @@ else{
   dirAr = false
 }
 
-console.log($('link[title=ltr]')[0])
-
-
-
+// loader
 $('#loading').fadeOut(500);
-// let screen = $(window).width();
-// console.log(screen);
-// $('.curve').css({
-//     "width":  + "" + screen,"height" : screen * .5,"border-top-left-radius":  (screen * 2),"border-top-right-radius": ($screen * 2)
-// })
-// $('.curve.top').css()
 
-/* -------------------------------- jquery -------------------------------- */ 
-// Dropdown Menu
-
+// owl carousel
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
+    $('.partners .owl-carousel').owlCarousel({
         loop:true,
-        margin:30,
-        nav:true,
-        dots:false,
-        navText: ["<i class='fa-solid fa-arrow-left-long'></i>","<i class='fa-solid fa-arrow-right-long'></i>"],
+        margin:10,
+        responsiveClass:true,
+        rtl:true,
+        autoplay:true,
+        nav:false,
+        dots: false,
         responsive:{
             0:{
-                items:1
+                items:2,
             },
             600:{
-                items:1
+                items:4,
             },
-            992:{
-                items:2
+            1000:{
+                items:6,
             }
         }
-    })
-});
-
-var navbar = document.getElementsByClassName("navbar");
-var sticky = navbar[0].offsetHeight ;
-
-
-// make nav bar static on scroll 
-window.addEventListener("scroll" , function(){
-    if (this.document.documentElement.scrollTop >= sticky) {
-        // $(navbar).css("position", "unset");
-        $(navbar).addClass("bg-main-color");
-    } else {
-        $(navbar).removeClass("bg-main-color")
-        // $(navbar).css("position", "sticky");
-    }
-})
-
-// make nav bar static on scroll 
-// if ($("#home").length > 0) {
-//     if (this.document.documentElement.scrollTop >= sticky) {
-//         $(navbar).addClass("bg-main-color");
-//     } else {
-//         $(navbar).removeClass("bg-main-color")
-//     }
-// }
-// else{
-//     $(navbar).addClass("bg-main-color")
-// }
-
-$('.partners .owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    responsiveClass:true,
-    rtl:true,
-    autoplay:true,
-    autoplayTimeout:20000,
-    nav:false,
-    dots: false,
-    responsive:{
-        0:{
-            items:2,
-        },
-        600:{
-            items:4,
-        },
-        1000:{
-            items:6,
+    });
+    
+    $("header .owl-carousel").owlCarousel({
+        nav: false,
+        autoplay: false,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        items: 1,
+        rtl: dirAr,
+        dots: true,
+        margin: 20
+    });
+    
+    $('.staff .owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        responsiveClass:true,
+        rtl:true,
+        autoplay:true,
+        nav:false,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:4,
+            }
         }
-    }
-});
-
-$("header .owl-carousel").owlCarousel({
-    nav: false,
-    autoplay: false,
-    autoplayHoverPause: true,
-    responsiveClass: true,
-    items: 1,
-    rtl: dirAr,
-    dots: true,
-    margin: 20
-  });
-
-  $('.staff .owl-carousel').owlCarousel({
-    loop:true,
-    margin:20,
-    responsiveClass:true,
-    rtl:true,
-    autoplay:true,
-    autoplayTimeout:20000,
-    nav:false,
-    responsive:{
-        0:{
-            items:1,
-        },
-        600:{
-            items:2,
-        },
-        1000:{
-            items:4,
+    });
+    
+    
+    $("#testimonial-slider").owlCarousel({
+        loop:true,
+        margin:10,
+        center: true,
+        responsiveClass:true,
+        rtl:true,
+        autoplay:true,
+        autoplayTimeout:20000,
+        nav:false,
+        rtl: dirAr,
+        items: 1.5,
+        responsive:{
+            0:{
+                items:1,
+            },
+            992:{
+                items:1.5,
+            }
         }
-    }
+    });
 });
 
-
-$("#testimonial-slider").owlCarousel({
-    loop:true,
-    margin:10,
-    center: true,
-    responsiveClass:true,
-    rtl:true,
-    autoplay:true,
-    autoplayTimeout:20000,
-    nav:false,
-    rtl: dirAr,
-    items: 1.5,
-    responsive:{
-        0:{
-            items:1,
-        },
-        992:{
-            items:1.5,
-        }
-    }
+// scroll to top page
+var btn_top = $('#scrollUp');
+$(window).scroll(function() {
+      if ($(window).scrollTop() > 300) {
+        btn_top.show();
+      } else {
+        btn_top.hide();
+      }
 });
 
+// services responsive
 $('.op-pro-filter').click(function () {
     $(this).toggleClass('active');
     $('.services-nav').slideToggle();
 });
 
+// dropdown menu
 var dropdown = document.querySelectorAll('.dropdown');
 var dropdownArray = Array.prototype.slice.call(dropdown,0);
 dropdownArray.forEach(function(el){
@@ -176,9 +130,7 @@ dropdownArray.forEach(function(el){
 		}
 	};
 })
-
 Element.prototype.hasClass = function(className) {
     return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
 };
 
-// $('select').niceSelect();
